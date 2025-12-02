171ded1b263ea9cc127d0bd0c55363111e3fbcb9
Juan <juanlowandy@gmail.com>
2025-12-02 18:13:38 +0700

    Read myfile.txt

--- src/week12_juan_117229/Read_file.java
+++ src/week12_juan_117229/Read_file.java
@@ -8,12 +8,16 @@ public class Read_file {
     public static void main(String[] args) {
 
         try {
-            FileWriter Writer = new FileWriter("myfile.txt");
-            Writer.write("Nama saya John Doe\n");
-            Writer.write("Saya dari Universitas Multimedia Nusantara");
+            FileWriter Writer 
+                = new FileWriter("myfile.txt");
+            Writer.write(
+            	"Nama saya John Doe\n");
+            Writer.write(
+            		"Saya dari Universitas Multimedia Nusantara");
             Writer.close();
             System.out.println("Successfully written.");
-        } catch (IOException e) {
+        } 
+        catch (IOException e) {
             System.out.println("An error has occurred.");
             e.printStackTrace();
         }
